7698ef21706f03e7fe2096f386c16cc784cffd6e
rhcp <rhcp@root.rhcp011235.me>
2017-12-16 22:54:18 -0500

    Add pull script for webhook

---
+++ pull.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# Pull the new changes to the blog
+/usr/bin/git pull
+
+#echo "A new archive has been created at /archives/${2}-master.tar" | mail -s 'Github project build successul!' 'your@email.com'
+
